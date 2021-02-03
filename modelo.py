4782630d92d88632ed2227b6eb8d5d4031b94333
GeovanaGalvao <gngalvao732@gmail.com>
2021-02-03 16:45:16 -0300

    Iniciando o uso de heranca

--- modelo.py
+++ modelo.py
@@ -1,9 +1,8 @@
-class Filme:
+class Programa:
 
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title  # Todas as palavras terão a primeira letra como maiuscula
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()  # Todas as palavras terão a primeira letra como maiuscula
         self.__ano = ano
-        self.__duracao = duracao  # Em minutos
         self.__like = 0
 
     @property
@@ -13,53 +12,39 @@ class Filme:
     @property
     def ano(self):
         return self.__ano
+
+    @property
+    def like(self):
+        return self.__like
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    def incrementa_like(self):
+        self.__like += 1
+
+
+class Filme(Programa):
+
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.__duracao = duracao  # Em minutos
 
     @property
     def duracao(self):
         return self.__duracao
 
-    @property
-    def like(self):
-        return self.__like
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title
+class Serie(Programa):
 
-    def incrementa_like(self):
-        self.__like += 1
-
-
-class Serie:
-
-    def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title
-        self.__ano = ano
-        self.__temporadas = temporadas
-        self.__like = 0
+    def __init__(self, nome, ano, quantidade_temporadas):
+        super().__init__(nome, ano)
+        self.__temporadas = quantidade_temporadas
 
     @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def ano(self):
-        return self.__ano
-
-    @property
-    def temporadas(self):
+    def temporada(self):
         return self.__temporadas
 
-    @property
-    def like(self):
-        return self.__like
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title
-
-    def incrementa_like(self):
-        self.__like += 1
-
     def incrementa_temporada(self):
-        self.__like += 1
+        self.__temporadas += 1
